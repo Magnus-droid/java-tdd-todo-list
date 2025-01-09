6c1ce75f738b51936f99fe3fb045d9f151f5bd98
Magnus-droid <magnuskrumbacher@gmail.com>
2025-01-09 13:50:53 +0100

    implement tests for Task class

---
+++ src/main/java/com/booleanuk/core/Task.java
@@ -0,0 +1,15 @@
+package com.booleanuk.core;
+
+public class Task {
+    String name;
+    boolean complete;
+
+    public Task(String name) {
+        this.name = name;
+        this.complete = false;
+
+    }
+
+    void changeStatus(boolean status) {
+    }
+}
