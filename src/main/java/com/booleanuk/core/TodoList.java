c25fbf856800b324702bd2300902481655755520
Magnus-droid <magnuskrumbacher@gmail.com>
2025-01-09 14:55:43 +0100

    Impemented searchTask + tests for it

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,5 +1,51 @@
 package com.booleanuk.core;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class TodoList {
+
+    ArrayList<Task> tasks = new ArrayList<>();
+
+
+    public boolean add(Task task) {
+        if (!tasks.contains(task)) {
+            tasks.add(task);
+            return true;
+        }
+        return false;
+    }
+
+    public ArrayList<Task> viewTasks() {
+        return tasks;
+    }
+
+    public ArrayList<Task> viewIncompleteTasks() {
+        ArrayList<Task> incomplete = new ArrayList<>();
+        for (Task task : this.tasks) {
+            if (!task.complete) {
+                incomplete.add(task);
+            }
+        }
+        return incomplete;
+    }
+
+    public ArrayList<Task> viewCompleteTasks() {
+        ArrayList<Task> complete = new ArrayList<>();
+        for (Task task : this.tasks) {
+            if (task.complete) {
+                complete.add(task);
+            }
+        }
+        return complete;
+    }
+
+    Task searchTask(Task task) {
+        if(tasks.contains(task)) {
+            return task;
+        }
+        System.out.println("Task not found!");
+        return null;
+    }
 
 }
--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -3,11 +3,64 @@ package com.booleanuk.core;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 class TodoListTest {
     @Test
-    public void exampleTest() {
-        String hello = "Hello";
-        Assertions.assertEquals("Hello", hello);
-        Assertions.assertNotEquals("Goodbye", hello);
+    public void checkForTaskInList() {
+        Task task = new Task("Test");
+        TodoList todo = new TodoList();
+        Assertions.assertTrue(todo.add(task));
+        Assertions.assertFalse(todo.add(task));
+
     }
+
+    @Test
+    public void checkViewTask() {
+        Task task = new Task("Test");
+        TodoList todo = new TodoList();
+        ArrayList<Task> empty = new ArrayList<>();
+        Assertions.assertIterableEquals(empty, todo.viewTasks());
+        todo.add(task);
+        empty.add(task);
+        Assertions.assertIterableEquals(empty, todo.viewTasks());
+
+    }
+
+    @Test
+    public void checkIncompleteTaskView() {
+        TodoList todo = new TodoList();
+        Task taskIncomplete = new Task("Task incomplete");
+        Task taskComplete = new Task("Task complete");
+        taskComplete.changeStatus(true);
+        todo.add(taskIncomplete);
+        todo.add(taskComplete);
+        Assertions.assertNotEquals(todo.viewTasks(), todo.viewIncompleteTasks());
+    }
+
+    @Test
+    public void checkCompleteTaskView() {
+        TodoList todo = new TodoList();
+        Task taskIncomplete = new Task("Task incomplete");
+        Task taskComplete = new Task("Task complete");
+        taskComplete.changeStatus(true);
+        todo.add(taskIncomplete);
+        todo.add(taskComplete);
+        Assertions.assertNotEquals(todo.viewTasks(), todo.viewCompleteTasks());
+    }
+
+    @Test
+    public void testSearchTaskName() {
+        TodoList todo = new TodoList();
+        Task example = new Task("Example");
+
+        Assertions.assertNotEquals(example, todo.searchTask(example));
+        todo.add(example);
+
+        Assertions.assertEquals(example, todo.searchTask(example));
+
+
+    }
+
 }
